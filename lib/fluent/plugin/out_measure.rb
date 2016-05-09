d95d73ba104591ab975bdad7c1b13b69fab11465
nemonium <kenichiro.nemoto@gmail.com>
2016-05-09 23:22:03 +0900

    fixed. #7
    Add unit test.

--- lib/fluent/plugin/out_measure.rb
+++ lib/fluent/plugin/out_measure.rb
@@ -1,9 +1,13 @@
+require 'logger'
+
 module Fluent
   class MeasureOutput < Output
     Plugin.register_output('measure', self)
 
     PRINT_FORMAT_EACH = "summary + %10d in %5ds = %10.3f/s Avg : %s"
     PRINT_FORMAT_ALL  = "summary = %10d in %5ds = %10.3f/s Avg"
+
+    attr_accessor :logger
 
     config_param :path, :string, :default => nil
     config_param :verbose, :bool, :default => true
@@ -12,7 +16,13 @@ module Fluent
     # This method is called before starting.
     def configure(conf)
       super
-      @enabled_path = @path.nil? ? false : FileTest.exist?(File.dirname(@path))
+      @logger = $log
+      if !@path.nil?
+        @logger = Logger.new("#{@path}", 'daily')
+        @logger.formatter = proc{|severity, datetime, progname, message|
+          "#{datetime} [measure]: #{message}\n"
+        }
+      end
     end
 
     def timer(arg, &proc)
@@ -25,78 +35,71 @@ module Fluent
       yield
     end
 
-    def printer(message)
-      if @enabled_path
-        File.open(@path, 'a') {|f| f.write "#{Time.now} [measure]: #{message}\n" }
-      else
-        $log.info message
-      end
+    def print_measure_result(latest_time, next_time, counter = @counter, verbose = @verbose)
+      time_range = next_time.to_i - latest_time.to_i
+      # measure results in unit of tag
+      tags_total = request_totals time_range, next_time, counter
+      # sum of measure results
+      total = 0
+      tags_total.each{|tag, value|
+        total += value
+        @logger.info "#{sprintf(PRINT_FORMAT_EACH, value, time_range, value.quo(time_range).to_f, tag)}" if verbose
+      }
+      @logger.info "#{sprintf(PRINT_FORMAT_ALL, total, time_range, total.quo(time_range).to_f)}"
     end
 
-    def print_measure(counter = @counter)
-      interval = 60
-      s_time = Time.now
-      next_time = s_time + interval - s_time.sec
-
-      loop do
-        $log.debug "next measure time [#{next_time}]"
-        timer(next_time) do
-          latency = next_time.to_i - s_time.to_i
-          $log.debug "measure range [#{s_time} - #{next_time - 1}]"
-
-          # measure in unit of tag
-          tags_total_val = Hash.new
-          latency.times do |i|
-            k = s_time.to_i + i
-            counter[k].each{|tag, value|
-              tags_total_val[tag] = 0 unless tags_total_val.has_key?(tag)
-              tags_total_val[tag] += value
-            } unless counter[k].nil?
-          end
-
-          # sum of measure results
-          total_val = 0
-          tags_total_val.each{|tag, value|
-            total_val += value
-            printer "#{sprintf(PRINT_FORMAT_EACH, value, latency, value.quo(latency).to_f, tag)}" if @verbose
-          }
-          printer "#{sprintf(PRINT_FORMAT_ALL, total_val, latency, total_val.quo(latency).to_f)}"
-        end
-        s_time = next_time
-        next_time += interval
+    def request_totals(time_range, base_time, counter = @counter)
+      rt = Hash.new
+      time_range.times do |i|
+        k = base_time.to_i - (i + 1)
+        counter[k].each{|tag, value|
+          rt[tag] = 0 unless rt.has_key?(tag)
+          rt[tag] += value
+        } unless counter[k].nil?
       end
+      return rt
     end
 
-    def expire_measure_history(expire = @expire, counter = @counter)
-      interval = 60
-      s_time = Time.now
-      next_time = s_time + interval - s_time.sec + 10
-
-      loop do
-        $log.debug "next buffer clean time [#{next_time}]"
-        timer(next_time) do
-          t = next_time - expire
-          $log.debug "expire time less than [#{t}]"
-
-          # clean buffer
-          counter.delete_if {|key, val| key < t.to_i }
-        end
-        next_time += interval
-      end
+    def clean_measure_buffer(base_time, counter = @counter, expire = @expire)
+      t = base_time - expire
+      # clean buffer
+      counter.delete_if {|key, val| key < t.to_i }
     end
 
     # This method is called when starting.
     def start
       super
-      @counter   = Hash.new
-      @t_measure = Thread.new { print_measure }
-      @t_expire  = Thread.new { expire_measure_history }
+      @counter = Hash.new
+      @t_print = Thread.new {
+        interval = 60
+        latest_time = Time.now
+        next_time = latest_time + interval - latest_time.sec
+        loop do
+          timer(next_time) do
+            print_measure_result latest_time, next_time
+          end
+          latest_time = next_time
+          next_time += interval
+        end
+      }
+      @t_clean = Thread.new {
+        interval = 60
+        latest_time = Time.now
+        next_time = latest_time + interval - latest_time.sec + 10
+        loop do
+          timer(next_time) do
+            clean_measure_buffer next_time
+          end
+          latest_time = next_time
+          next_time += interval
+        end
+      }
     end
 
     # This method is called when shutting down.
     def shutdown
-      Thread::kill(@t_measure) unless @t_measure.nil?
-      Thread::kill(@t_expire)  unless @t_expire.nil?
+      Thread::kill(@t_print) unless @t_print.nil?
+      Thread::kill(@t_clean) unless @t_clean.nil?
       super
     end
 
